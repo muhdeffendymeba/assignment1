ed80e487827ef839558086a65701b08ca59f4365
muhdeffendymeba <6146736Q@polite.edu.sg>
2026-05-20 16:26:27 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-fetch('https://dragonball-api.com/api/characters/')
-    method: "GET"
-    headers:{
-        "Content-Type": "application/json"
-    }
-    body: JSON.stringify()
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.data[1].name);
-    })
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-    .catch(error => console.error("Error", error));
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
---
+++ index1.js
@@ -0,0 +1,16 @@
+fetch('https://dragonball-api.com/api/characters/')
+    method: "GET"
+    headers:{
+        "Content-Type"; "application/json"
+    }
+    body: JSON.stringify()
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.data[1].name);
+    })
+
+    .catch(error => console.error("Error", error));
+
+
+
+     
